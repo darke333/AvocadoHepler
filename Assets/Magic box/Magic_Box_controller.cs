8371aeb8cdc855e042ef2321be1d1de4e5f7723c
Alex <sanbestik@yandex.ru>
2020-11-07 16:23:36 +0300

    New Audio and 3D models

---
+++ Assets/Magic box/Magic_Box_controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic_Box_controller : MonoBehaviour
+{
+    public AudioClip [] audioClip  = new AudioClip [10];
+    public GameObject[] gameObject = new GameObject[10];
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
