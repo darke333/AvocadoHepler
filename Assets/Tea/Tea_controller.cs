a2fc0ff85498488c8aa2174f364475754e02161c
Alex <sanbestik@yandex.ru>
2020-11-07 21:34:45 +0300

    Add Tea inventory

---
+++ Assets/Tea/Tea_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tea_controller : MonoBehaviour
+{
+    public GameObject[] Inventar = new GameObject[12];
+
+    void Start()
+    {
+        for (int i=0; i<12; i++)
+        {
+            Instantiate(Inventar[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
