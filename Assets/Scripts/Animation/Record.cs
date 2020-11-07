5e8c6f07f3ce853653bae2e14f3afbb1c1aab29f
SirotenkoMax <sirotenko.max1998@gmail.com>
2020-11-07 12:44:47 +0300

    WorkingOnAnimations

---
+++ Assets/Scripts/Animation/Record.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+
+public class Record : MonoBehaviour
+{
+    public AnimationClip clip;
+
+    private GameObjectRecorder m_Recorder;
+
+    public bool Recording;
+
+    void Start()
+    {
+        // Create recorder and record the script GameObject.
+        m_Recorder = new GameObjectRecorder(gameObject);
+
+        // Bind all the Transforms on the GameObject and all its children.
+        m_Recorder.BindComponentsOfType<Transform>(gameObject, true);
+    }
+
+    void LateUpdate()
+    {
+        if (clip == null)
+            return;
+
+        if (Recording)
+        {
+            m_Recorder.TakeSnapshot(Time.deltaTime);
+
+        }
+        else if (m_Recorder.isRecording)
+        {
+            // Save the recorded session to the clip.
+            m_Recorder.SaveToClip(clip);
+        }
+
+        // Take a snapshot and record all the bindings values for this frame.
+    }
+
+    void OnDisable()
+    {
+        /*
+        if (clip == null)
+            return;
+
+
+        if (m_Recorder.isRecording)
+        {
+            // Save the recorded session to the clip.
+            m_Recorder.SaveToClip(clip);
+        }*/
+    }
+}
